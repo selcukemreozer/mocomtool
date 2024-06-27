cedfa0022a0da475b90e362d827cabb6c082205f
Selcuk Emre Özer <selcuk.emre.ozer@outlook.com>
2024-06-27 03:13:40 +0300

    mocom file was created

--- CLItest.py
+++ CLItest.py
@@ -1,9 +1,3 @@
-from openai import OpenAI
-import anthropic
-import google.generativeai as genai
-import PIL.Image
-import os
-
 import typer
 
 app = typer.Typer()
---
+++ mocom.py
@@ -0,0 +1,9 @@
+from openai import OpenAI
+import anthropic
+import google.generativeai as genai
+import PIL.Image
+import os
+import typer
+
+app = typer.Typer()
+
